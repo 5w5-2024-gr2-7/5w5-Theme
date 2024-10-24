9254a6a8a031f5aaf30c688a49b6c4539bf55e1f
mathcdu <mathieucdufour@gmail.com>
2024-10-23 22:57:55 -0400

    création header et footer php ainsi que le function. fonctionnel.

---
+++ footer.php
@@ -0,0 +1,49 @@
+<footer>
+    <div class="partenaires">
+      <h2>Nos partenaires</h2>
+      <div class="partenaires-images">
+        <img src="images/Beslogic.jpg" alt="Partenaire 1">
+        <img src="images/Sonos.webp" alt="Partenaire 2">
+        <img src="images/technomontreal-og.jpg" alt="Partenaire 3">
+      </div>
+    </div>
+
+    <div class="footer-sections">
+      <div class="footer-section">
+        <h3>Nous retrouver</h3>
+        <ul>
+          <li>Instagram</li>
+          <li>Facebook</li>
+          <li>Twitter</li>
+        </ul>
+      </div>
+
+      <div class="footer-section">
+        <h3>Naviguer</h3>
+        <ul>
+          <li>Stages</li>
+          <li>Professeurs</li>
+          <li>Cours</li>
+          <li>Projets</li>
+        </ul>
+      </div>
+
+      <div class="footer-section">
+        <h3>À propos</h3>
+        <ul>
+          <li>Collège de Maisonneuve</li>
+          <li>3800 Sherbrooke St E, Montreal, Quebec H1X 2A2</li>
+          <li>514 254 7131</li>
+        </ul>
+      </div>
+    </div>
+
+    <p>Crédits - Tous droits réservés à Alexander Rankov, Dominic Gibeau, Guillaume Ducharme-Vanier et Mathieu
+      Croteau-Dufour © 2024</p>
+  </footer>
+  <?php
+wp_footer();
+?>
+</body>
+
+</html>
