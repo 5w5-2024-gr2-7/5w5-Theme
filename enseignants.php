f2a60af46fc5a3be0d895141a3ab19c45e0cb72d
mathcdu <mathieucdufour@gmail.com>
2024-10-29 22:10:51 -0400

    Page professeur détaillé complété

--- enseignants.php
+++ enseignants.php
@@ -7,10 +7,9 @@ get_header(); ?>
     <h1>Les Profs du TIM</h1>
     <div class="professor-container">
         <?php
-        // Query to get all professors
         $query = new WP_Query(array(
-            'post_type' => 'professor', // Your custom post type
-            'posts_per_page' => -1 // Fetch all professors
+            'post_type' => 'professor', 
+            'posts_per_page' => -1
         ));
 
         if ($query->have_posts()) :
@@ -20,12 +19,12 @@ get_header(); ?>
                     <?php if (has_post_thumbnail()) : ?>
                         <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="<?php the_title(); ?>">
                     <?php else : ?>
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/default-image.png" alt="Default Image"> <!-- Fallback image -->
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/default-image.png" alt="Default Image">
                     <?php endif; ?>
                     <button><a href="<?php the_permalink(); ?>">En savoir plus</a></button>
                 </div>
             <?php endwhile;
-            wp_reset_postdata(); // Reset the post data
+            wp_reset_postdata();
         else : ?>
             <p>Aucun professeur trouvé.</p>
         <?php endif; ?>
