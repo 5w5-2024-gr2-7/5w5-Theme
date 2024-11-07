3e5f51c34406d8c6925dd5c7239425930ba6fdf0
Alexander Rankov <e2259198@cmaisonneuve.qc.ca>
2024-11-07 14:29:42 -0500

    07 novembre 2024: J'ai commencé à travailler sur le PHP de la page stages. J'ai aussi créé une fonction pour générer du contenu sur Wordpress

--- functions.php
+++ functions.php
@@ -93,3 +93,20 @@ function add_menu_class($items) {
     return $items;
 }
 add_filter('wp_nav_menu_objects', 'add_menu_class');
+
+// Section des stages
+function create_stages_post_type() {
+    register_post_type('stages',
+        array(
+            'labels' => array(
+                'name' => __('Stages'),
+                'singular_name' => __('Stage')
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'stages'),
+            'supports' => array('title', 'editor', 'thumbnail'),
+        )
+    );
+}
+add_action('init', 'create_stages_post_type');
--- stages.php
+++ stages.php
@@ -1,1 +1,48 @@
-<?php ?>
+<?php get_header(); ?>
+
+<main>
+    <section class="stages-section">
+        <h1>Les stages</h1>
+
+        <div class="filtration-stage">
+            <div class="stage-type">
+                <div class="volet-dropdown">
+                    <span>Type</span>
+                    <select>
+                        <option value="web">Design</option>
+                        <option value="jeux">Jeux</option>
+                        <option value="graphisme">Web</option>
+                        <option value="graphisme">Vidéo</option>
+                    </select>
+                </div>
+            </div>
+            <div class="stage-company">
+                <div class="volet-dropdown">
+                    <span>Compagnie</span>
+                    <select>
+                        <option value="ubisoft">Ubisoft</option>
+                        <option value="bombardier">Bombardier</option>
+                        <option value="uptimum">Groupe Uptimum</option>
+                        <option value="repentigny">Ville de Repentigny</option>
+                        <option value="ubisoft2">Ubisoft 2</option>
+                        <option value="indexdesign">Index-Design</option>
+                        <option value="princecharles">Prince Charles</option>
+                        <option value="sinep">Sinep</option>
+                        <option value="lipari">Lipari design</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="stage-details">
+            <h1>Ubisoft</h1>
+
+            <div class="description-stage">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <img src="<?php echo get_template_directory_uri(); ?>/images/Apropos.jpg" alt="Logo Ubisoft">
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
