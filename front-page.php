3216f3780c997d8e7ada4bb2fc7de484b0613d29
Guillaume Ducharme-Vanier <guiducvan@videotron.ca>
2024-11-22 23:40:36 -0500

    avancement page accueil

--- front-page.php
+++ front-page.php
@@ -10,9 +10,8 @@ get_header();
         <h1 class="titre-principale">
             Où la <span class="titre-principale-italique">création</span><br>commencee
         </h1>
-
         <div>
-            
+            <!-- <img src="<?php echo site_url('/wp-content/uploads/2024/11/BouleRougegif.gif'); ?>" alt="Animation abstraite"> -->
         </div>
     </div>
     <!-- <div class="Swirl">
@@ -32,23 +31,71 @@ get_header();
 
     <header>
       <div class="titre-principal">
-        <h2>À propos</h2>
+      <h2 class="h2-info">À propos</h2>
+
+       
+
       </div>
     </header>
 
     <div class="contenu-about">
+    <h3 class="h3-info">Technique d'intégration multimédia ?</h3>
+    <div class="image-info">
       <div class="image-apropos">
-        <img src="<?php echo get_site_url(); ?>/wp-content/uploads/2024/11/Apropos.jpg" alt="À propos">
+        <img src="<?php echo get_site_url(); ?>/wp-content/uploads/2024/11/file.jpg" alt="À propos">
       </div>
       <div class="about-contenu">
-        <p>Cette formation de trois ans prépare les étudiants à relever les défis du secteur technologique et créatif en constante évolution...</p>
+      <h3 class="h3-info2">Technique d'intégration multimédia ?</h3>
+        <p>Cette formation de trois ans prépare les étudiants à relever les défis d’un secteur <span class="mot-couleur">technologique</span> et <span class="mot-couleur">créatif</span> en constante évolution. Axé sur la <span class="mot-couleur">technique</span>, ce programme encourage les étudiants à expérimenter concrètement par eux-mêmes, favorisant ainsi un apprentissage <span class="mot-couleur">davantage pratique</span> que théorique.
+            Mêlant création et innovation, ce programme englobe plusieurs aspects qui se retrouvent dans l'utilisation courante. <br><br>
+             Il s'agit d’un milieu en croissance qui demande de plus en plus de relève, afin de relever les idées technologiques de demain. Serez-vous le <?php echo do_shortcode('[compteur_visites]'); ?>ᵉ à rejoindre cette aventure ? Si l’acquisition des <span class="mot-couleur">compétences</span> suivantes vous intéresse, vous êtes au bon endroit :
+        </p>
+        </div>
+        </div>
         <div class="info-additionnel">
-          <h3>Conception graphique</h3>
-          <h3>Programmation et développement web</h3>
-          <h3>Production audiovisuelle</h3>
-          <h3>Gestion de projet multimédia</h3>
-          <h3>Intégration et optimisation de solutions interactives</h3>
-          <h3><?php echo do_shortcode('[compteur_visites]'); ?></h3>
+
+            <div class="info-additionnel-section">
+                <svg class="info-additionnel-section-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M208 32c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32l0 140.9 122-70.4c15.3-8.8 34.9-3.6 43.7 11.7l16 27.7c8.8 15.3 3.6 34.9-11.7 43.7L352 256l122 70.4c15.3 8.8 20.6 28.4 11.7 43.7l-16 27.7c-8.8 15.3-28.4 20.6-43.7 11.7L304 339.1 304 480c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-140.9L86 409.6c-15.3 8.8-34.9 3.6-43.7-11.7l-16-27.7c-8.8-15.3-3.6-34.9 11.7-43.7L160 256 38 185.6c-15.3-8.8-20.5-28.4-11.7-43.7l16-27.7C51.1 98.8 70.7 93.6 86 102.4l122 70.4L208 32z"/>
+                </svg>
+                <h3 >Conception graphique</h3>
+            </div>
+
+            <div class="info-additionnel-section">
+                <svg class="info-additionnel-section-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M208 32c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32l0 140.9 122-70.4c15.3-8.8 34.9-3.6 43.7 11.7l16 27.7c8.8 15.3 3.6 34.9-11.7 43.7L352 256l122 70.4c15.3 8.8 20.6 28.4 11.7 43.7l-16 27.7c-8.8 15.3-28.4 20.6-43.7 11.7L304 339.1 304 480c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-140.9L86 409.6c-15.3 8.8-34.9 3.6-43.7-11.7l-16-27.7c-8.8-15.3-3.6-34.9 11.7-43.7L160 256 38 185.6c-15.3-8.8-20.5-28.4-11.7-43.7l16-27.7C51.1 98.8 70.7 93.6 86 102.4l122 70.4L208 32z"/>
+                </svg>
+                <h3>Création de Jeux-Vidéo</h3>
+            </div>
+
+            <div class="info-additionnel-section">
+                <svg class="info-additionnel-section-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M208 32c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32l0 140.9 122-70.4c15.3-8.8 34.9-3.6 43.7 11.7l16 27.7c8.8 15.3 3.6 34.9-11.7 43.7L352 256l122 70.4c15.3 8.8 20.6 28.4 11.7 43.7l-16 27.7c-8.8 15.3-28.4 20.6-43.7 11.7L304 339.1 304 480c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-140.9L86 409.6c-15.3 8.8-34.9 3.6-43.7-11.7l-16-27.7c-8.8-15.3-3.6-34.9 11.7-43.7L160 256 38 185.6c-15.3-8.8-20.5-28.4-11.7-43.7l16-27.7C51.1 98.8 70.7 93.6 86 102.4l122 70.4L208 32z"/>
+                </svg>
+                <h3>Gestion de projet multimédia</h3>
+            </div>
+
+            <div class="info-additionnel-section">
+                <svg class="info-additionnel-section-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M208 32c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32l0 140.9 122-70.4c15.3-8.8 34.9-3.6 43.7 11.7l16 27.7c8.8 15.3 3.6 34.9-11.7 43.7L352 256l122 70.4c15.3 8.8 20.6 28.4 11.7 43.7l-16 27.7c-8.8 15.3-28.4 20.6-43.7 11.7L304 339.1 304 480c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-140.9L86 409.6c-15.3 8.8-34.9 3.6-43.7-11.7l-16-27.7c-8.8-15.3-3.6-34.9 11.7-43.7L160 256 38 185.6c-15.3-8.8-20.5-28.4-11.7-43.7l16-27.7C51.1 98.8 70.7 93.6 86 102.4l122 70.4L208 32z"/>
+                </svg>
+                <h3>Modelisation et animation 3D</h3>
+            </div>
+
+            <div class="info-additionnel-section">
+                <svg class="info-additionnel-section-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M208 32c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32l0 140.9 122-70.4c15.3-8.8 34.9-3.6 43.7 11.7l16 27.7c8.8 15.3 3.6 34.9-11.7 43.7L352 256l122 70.4c15.3 8.8 20.6 28.4 11.7 43.7l-16 27.7c-8.8 15.3-28.4 20.6-43.7 11.7L304 339.1 304 480c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-140.9L86 409.6c-15.3 8.8-34.9 3.6-43.7-11.7l-16-27.7c-8.8-15.3-3.6-34.9 11.7-43.7L160 256 38 185.6c-15.3-8.8-20.5-28.4-11.7-43.7l16-27.7C51.1 98.8 70.7 93.6 86 102.4l122 70.4L208 32z"/>
+                </svg>
+                <h3>Programmation et développement Web</h3>
+            </div>
+
+            <div class="info-additionnel-section">
+                <svg class="info-additionnel-section-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M208 32c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32l0 140.9 122-70.4c15.3-8.8 34.9-3.6 43.7 11.7l16 27.7c8.8 15.3 3.6 34.9-11.7 43.7L352 256l122 70.4c15.3 8.8 20.6 28.4 11.7 43.7l-16 27.7c-8.8 15.3-28.4 20.6-43.7 11.7L304 339.1 304 480c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-140.9L86 409.6c-15.3 8.8-34.9 3.6-43.7-11.7l-16-27.7c-8.8-15.3-3.6-34.9 11.7-43.7L160 256 38 185.6c-15.3-8.8-20.5-28.4-11.7-43.7l16-27.7C51.1 98.8 70.7 93.6 86 102.4l122 70.4L208 32z"/>
+                </svg>
+                <h3>Intégration et optimisation de solutions interactives</h3>
+            </div>         
+          
           
         </div>
       </div>
