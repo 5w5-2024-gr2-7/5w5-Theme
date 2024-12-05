5481bc8669a50f04e761460cf4c8e072847d3e9b
Guillaume Ducharme-Vanier <guiducvan@videotron.ca>
2024-12-05 18:57:14 -0500

    CSS de la page 404

--- 404.php
+++ 404.php
@@ -2,11 +2,16 @@
 
 <main class="page-404">
     <div class="container">
-        <h1 class="error-title">404 - Page introuvable</h1>
-        <p class="error-message">Oups! La page que vous recherchez n'existe pas ou a été déplacée.</p>
-        <img src="<?php echo get_template_directory_uri(); ?>/images/404-illustration.png" alt="Illustration de 404" class="error-image" />
+        <div class="titre-404">
+        <h1 class="error-title">Désolé, cette page n'existe pas<i class="fa-solid fa-face-frown"></i></h1>
+        </div>
+        
+        <p class="error-message">Oups! La page que vous recherchez n'existe pas ou a été déplacée. Retournons sur le bon chemin ensemble voulez-vous.</p>
+        <button class="error-boutton"><a href="<?php echo home_url(); ?>">Page d'accueil</a></button>
+        <img class="error-img" src="<?php echo get_site_url(); ?>/wp-content/uploads/2024/12/image.png" alt="À propos">
 
-        <div class="error-links">
+        
+        <!-- <div class="error-links">
             <p>Essayez de revenir à l'une des pages principales :</p>
             <ul>
                 <li><a href="<?php echo home_url(); ?>">Page d'accueil</a></li>
@@ -15,7 +20,7 @@
                 <li><a href="<?php echo get_post_type_archive_link('project'); ?>">Projets</a></li>
                 <li><a href="<?php echo site_url('/contact'); ?>">Contactez-nous</a></li>
             </ul>
-        </div>
+        </div> -->
     </div>
 </main>
 
--- archive-course.php
+++ archive-course.php
@@ -58,6 +58,16 @@
             <p>Aucun cours trouvé</p>
         <?php endif; ?>
     </div>
+
+    <svg id="sw-js-blob-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">
+                <stop id="stop1" stop-color="rgba(55, 218.58, 248, 0.37)" offset="0%"></stop>
+                <stop id="stop2" stop-color="rgba(251, 31, 150.704, 0.34)" offset="100%"></stop>
+            </linearGradient>
+        </defs>
+        <path fill="url(#sw-gradient)" d="M18.9,-28.6C23.7,-22.5,26.2,-15.9,30,-8.7C33.8,-1.5,38.9,6.4,37.3,12.3C35.6,18.2,27.3,22.2,20,27.1C12.7,32,6.3,37.9,-0.8,39C-8,40.1,-16,36.6,-20.1,30.6C-24.3,24.7,-24.7,16.4,-24.7,9.6C-24.8,2.8,-24.5,-2.5,-24,-8.9C-23.4,-15.2,-22.6,-22.6,-18.5,-28.9C-14.5,-35.3,-7.2,-40.6,-0.1,-40.4C7,-40.3,14.1,-34.7,18.9,-28.6Z" width="100%" height="100%" transform="translate(50 50)" stroke-width="0" style="transition: 0.3s;" stroke="url(#sw-gradient)"></path>
+    </svg>
 </section>
 
 <?php get_footer(); ?>
